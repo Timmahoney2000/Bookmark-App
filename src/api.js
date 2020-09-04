21fbdc71c56425fb5ca9d0dc8288ff6b170a90f4
Tim Mahoney <timmahoney2000@gmail.com>
2020-09-04 15:02:02 -0400

    add package.json

---
+++ src/api.js
@@ -0,0 +1,29 @@
+// function getBookmarks() {
+//     fetch('https://thinkful-list-api.herokuapp.com/timm/bookmarks')
+//         .then(response => {
+//         if (response.ok) {
+//             return response.json();
+//         }
+//         throw new Error(response.statusText);
+//         })
+//         .then(responseJson => displayResults(responseJson))
+//         .catch(err => {
+//         $('#js-error-message').text(`Something went wrong: ${err.message}`);
+//         });
+//         console.log(responseJson)
+// };
+
+// function postBookmarks
+//     const bookmark = JSON.stringify({
+//         id: {Math.random,
+//         title: "plumbing hard",
+//         description: "chai tea bing bang wahoo plumbing all day swanging all night",
+//         url: "hffbs://itsAme.luihio",
+//         rating: 5,
+//         expanded: true,
+
+//     })
+
+// // function update
+
+// // function delete
