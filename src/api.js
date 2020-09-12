4d15ad0685bdce221c23a18f7319d11b02aa2b9d
Tim Mahoney <timmahoney2000@gmail.com>
2020-09-12 00:03:49 -0400

    9-11-20

--- src/api.js
+++ src/api.js
@@ -1,29 +1,85 @@
-// function getBookmarks() {
-//     fetch('https://thinkful-list-api.herokuapp.com/timm/bookmarks')
-//         .then(response => {
-//         if (response.ok) {
-//             return response.json();
-//         }
-//         throw new Error(response.statusText);
-//         })
-//         .then(responseJson => displayResults(responseJson))
-//         .catch(err => {
-//         $('#js-error-message').text(`Something went wrong: ${err.message}`);
-//         });
-//         console.log(responseJson)
-// };
+'use strict';
 
-// function postBookmarks
-//     const bookmark = JSON.stringify({
-//         id: {Math.random,
-//         title: "plumbing hard",
-//         description: "chai tea bing bang wahoo plumbing all day swanging all night",
-//         url: "hffbs://itsAme.luihio",
-//         rating: 5,
-//         expanded: true,
+//base url api = thinkful api + my name/bookmarks
+let baseUrl = 'https://thinkful-list-api.herokuapp.com/tim/bookmarks';
 
-//     })
 
-// // function update
+//function bigFetch is the main fetch function
+function bigFetch(...args){
+    //variable error for this if statement if fetch fails
+  let error;
+  return fetch(...args)
+  .then(res => {
+    if (!res.ok){
+      error= { code: res.status };
+    }
+    return res.json();
+  })
+  .then(data => {
+    if (error) {
+      //error message returned to user if fetch fails
+      error.message = data.message;
+      return Promise.reject(error);
+    }
+    return data;
+  })
+  //catch block with console.log. was having issues with this earlier, seems to be resolved
+  .catch(error =>{console.log(error)})
+  
+}
 
-// // function delete
+//GET for returning to bookmarks using bigFetch function for api call
+function getBookmark() {
+  return bigFetch(`${baseUrl}`)
+  
+}
+
+// POST fetch
+//POST for adding a new bookmark & using bigFetch function for confirmation
+function newBookmark(title, url, desc, rating){
+    //newItem function to update JSON object
+  let newItem = JSON.stringify({
+     title: title,
+     url: url,
+     desc: desc,
+     rating: rating 
+  })
+  return bigFetch(`${baseUrl}`, {
+    method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: newItem
+  })
+  
+}
+
+//DELETE for removing bookmarks & bigFetch function for confirmation
+function deleteBookmark(id){
+  return bigFetch(`${baseUrl}/${id}`, {
+    method: "DELETE"
+  })
+};
+
+//PATCH for updating bookmarks & bigFetch function for confirmation
+function changeBookmark(id, title, url, desc, rating){
+    //newItem function to update JSON object
+  let newItem = JSON.stringify({
+    title: title,
+    url: url,
+    desc: desc,
+    rating: rating 
+ })
+ return bigFetch(`${baseUrl}/${id}`, {
+  method: "PATCH",
+      headers: {"Content-Type": "application/json"},
+      body: newItem
+})
+
+}
+//exports all functions to related modules
+export default{
+    
+  getBookmark,
+  newBookmark,
+  deleteBookmark,
+  changeBookmark
+}
